13e49e421e577be5014b115b53bd6d98edbdbb5b
m-maqshm <160467214+m-maqshm@users.noreply.github.com>
2024-08-25 12:39:50 +0300

    edit function loade Prodect

--- controller/product.dart
+++ controller/product.dart
@@ -9,5 +9,10 @@ class ProductController {
   DataBase dbOject = DataBase.instance;
   loadeProdect() {
     List<Map<String, dynamic>> data = dbOject.getData("products");
+    List<Product> allPrdects = data
+        .map((x) => Product(
+            name: x['name'], price: x["price"], qty: x["qty"], image: ''))
+        .toList();
+    productsView.showproduct(allPrdects);
   }
 }
